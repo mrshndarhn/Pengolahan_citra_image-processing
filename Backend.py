9dc89821dba36e89752ef16c64885304112f5e0a
ADITYA IMAM ZUHDI <100418056+adityaimamz@users.noreply.github.com>
2023-12-21 23:27:05 +0700

    add new filter

--- Backend.py
+++ Backend.py
@@ -2,76 +2,203 @@ import cv2
 import numpy as np
 from PIL import Image
 
-#----------Image to display--------------
-
+# Melakukan transformasi warping affine pada gambar
 def warpaffine(image):
-    rows, cols, ch = image.shape
-    pts1 = np.float32([[50, 50],
-                       [200, 50],
-                       [50, 200]])
-    pts2 = np.float32([[50, 100],
-                       [200, 50],
-                       [150, 200]])
-    points = cv2.getAffineTransform(pts1, pts2)
-    img = cv2.wrapAffine(image, points, (cols, rows))
-    img_conv = Image.fromarray(img)
-    return img_conv
+    try:
+        rows, cols, ch = image.shape
+        pts1 = np.float32([[50, 50],
+                           [200, 50],
+                           [50, 200]])
+        pts2 = np.float32([[50, 100],
+                           [200, 50],
+                           [150, 200]])
+        points = cv2.getAffineTransform(pts1, pts2)
+        img = cv2.wrapAffine(image, points, (cols, rows))
+        img_conv = Image.fromarray(img)
+        return img_conv
+    except Exception as e:
+        print(f"Error: {e}. Terjadi kesalahan saat melakukan transformasi warping affine pada gambar.")
 
-#------------Edge Detection-----------
-
+# Melakukan deteksi tepi pada gambar
 def edge_detection(image):
-    edges = cv2.Canny(image, 40, 80, L2gradient=True)
-    img_conv = Image.fromarray(edges)
-    return img_conv
+    try:
+        edges = cv2.Canny(image, 40, 80, L2gradient=True)
+        img_conv = Image.fromarray(edges)
+        return img_conv
+    except Exception as e:
+        print(f"Error: {e}. Terjadi kesalahan saat melakukan deteksi tepi.")
 
-#----------Gray Scale----------------
-
+# Melakukan konversi ke skala abu-abu
 def gray_scale(image):
-    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    img_conv = Image.fromarray(gray_image)
-    return img_conv
+    try:
+        gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        img_conv = Image.fromarray(gray_image)
+        return img_conv
+    except Exception as e:
+        print(f"Error: {e}. Terjadi kesalahan saat melakukan konversi ke skala abu-abu.")
 
-#---------Negative Transformation----------
-
+# Melakukan transformasi negatif pada gambar
 def negative_transformation(image):
-    height, width, _ = image.shape
-    for i in range(0, height - 1):
-        for j in range(0, width - 1):
-            pixel = image[i, j]
-            pixel[0] = 255 - pixel[0]
-            pixel[1] = 255 - pixel[1]
-            pixel[2] = 255 - pixel[2]
-    return image
+    try:
+        height, width, _ = image.shape
+        for i in range(0, height - 1):
+            for j in range(0, width - 1):
+                try:
+                    pixel = image[i, j]
+                    pixel[0] = 255 - pixel[0]
+                    pixel[1] = 255 - pixel[1]
+                    pixel[2] = 255 - pixel[2]
+                except IndexError as e:
+                    print(f"Error: {e}. Pastikan ukuran gambar cukup besar.")
+        return image
+    except Exception as e:
+        print(f"Error: {e}. Terjadi kesalahan saat melakukan transformasi negatif.")
 
-#--------Gaussian Blur--------------
-
+# Melakukan blur Gaussian pada gambar
 def Gaussian_Blur(image):
-    # image boundary (BORDER_DEFAULT)
-    Blur = cv2.GaussianBlur(image, (5, 5), cv2.BORDER_DEFAULT)
-    img_conv = Image.fromarray(Blur)
-    return img_conv
+    try:
+        blur = cv2.GaussianBlur(image, (5, 5), cv2.BORDER_DEFAULT)
+        img_conv = Image.fromarray(blur)
+        return img_conv
+    except Exception as e:
+        print(f"Error: {e}. Terjadi kesalahan saat melakukan blur Gaussian.")
 
-#----------Reduce noise----------
-
+# Mengurangi noise pada gambar
 def reduce_noise(image):
-    '''InputArray src,
-	OutputArray dst,
-	float h = 20,
-	float hColor = 20,
-	int templateWindowSize = 7,
-	int searchWindowSize = 21'''
-    #NON-LOCAL MEANS DENOISING ALGOROTHM
-    noiseless_image_colored = cv2.fastNlMeansDenoisingColored(image, None, 20, 20, 7, 21)
-    img_conv = Image.fromarray(noiseless_image_colored)
-    return img_conv
+    try:
+        # ALGORITMA PENGURANGAN NOISE NON-LOCAL MEANS
+        noiseless_image_colored = cv2.fastNlMeansDenoisingColored(image, None, 20, 20, 7, 21)
+        img_conv = Image.fromarray(noiseless_image_colored)
+        return img_conv
+    except Exception as e:
+        print(f"Error: {e}. Terjadi kesalahan saat mengurangi noise pada gambar.")
 
-#------------Sharping-------------------
-
+# Melakukan sharpening pada gambar
 def sharp_image(image):
-    kernel = np.array([[0, -1, 0],
-                       [-1, 5, -1],
-                       [0, -1, 0]])
-    image_sharp = cv2.filter2D(src=image, ddepth=-1, kernel=kernel)
-    img_conv = Image.fromarray(image_sharp)
-    return img_conv
+    try:
+        kernel = np.array([[0, -1, 0],
+                           [-1, 5, -1],
+                           [0, -1, 0]], dtype=np.float32)
+        image_sharp = cv2.filter2D(src=image, ddepth=-1, kernel=kernel)
+        img_conv = Image.fromarray(image_sharp)
+        return img_conv
+    except Exception as e:
+        print(f"Error: {e}. Terjadi kesalahan saat melakukan sharpening pada gambar.")
 
+#------------Embosis-------------------
+
+def emboss(image):
+    try:
+        kernel = np.array([[-2, -1, 0],
+                           [-1, 1, 1],
+                           [0, 1, 2]])
+        image_emboss = cv2.filter2D(src=image, ddepth=-1, kernel=kernel)
+        img_conv = Image.fromarray(image_emboss)
+        return img_conv
+    except Exception as e:
+        print(f"Error: {e}. Terjadi kesalahan saat melakukan emboss pada gambar.")
+
+#------------High-Boost-------------------
+
+def high_boost(image, alpha=2.0):
+    try:
+        # High-boost filtering = (alpha - 1) * original_image - alpha * low-pass_filtered_image
+        low_pass = cv2.GaussianBlur(image.astype(np.float64), (5, 5), 0)
+        high_boost_image = alpha * image.astype(np.float64) - (alpha - 1) * low_pass
+        # Konversi kembali ke tipe data uint8 untuk kompatibilitas dengan Image.fromarray
+        high_boost_image = np.clip(high_boost_image, 0, 255).astype(np.uint8)
+        img_conv = Image.fromarray(high_boost_image)
+        return img_conv
+    except Exception as e:
+        print(f"Error: {e}. Terjadi kesalahan saat melakukan high-boost filtering pada gambar.")
+
+#------------Low-Pass-------------------
+
+def low_pass(image):
+    try:
+        low_pass_image = cv2.GaussianBlur(image, (5, 5), 0)
+        img_conv = Image.fromarray(low_pass_image)
+        return img_conv
+    except Exception as e:
+        print(f"Error: {e}. Terjadi kesalahan saat melakukan low-pass filtering pada gambar.")
+
+# Rotasi
+def rotation(image, angle):
+    try:
+        rows, cols, ch = image.shape
+        M = cv2.getRotationMatrix2D((cols/2, rows/2), angle, 1)
+        rotated_image = cv2.warpAffine(image, M, (cols, rows))
+        img_conv = Image.fromarray(rotated_image)
+        return img_conv
+    except Exception as e:
+        print(f"Error: {e}. Terjadi kesalahan saat melakukan rotasi pada gambar.")
+
+# Translasi
+def translation(image, x, y):
+    try:
+        rows, cols, ch = image.shape
+        M = np.float32([[1, 0, x], [0, 1, y]])
+        translated_image = cv2.warpAffine(image, M, (cols, rows))
+        img_conv = Image.fromarray(translated_image)
+        return img_conv
+    except Exception as e:
+        print(f"Error: {e}. Terjadi kesalahan saat melakukan translasi pada gambar.")
+
+# Countour
+def contour(image):
+    try:
+        contours, _ = cv2.findContours(image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        cv2.drawContours(image, contours, -1, (0, 255, 0), 3)
+        img_conv = Image.fromarray(image)
+        return img_conv
+    except Exception as e:
+        print(f"Error: {e}. Terjadi kesalahan saat melakukan countour pada gambar.")
+
+# Invers
+def inverse(image):
+    try:
+        inverted_image = cv2.bitwise_not(image)
+        img_conv = Image.fromarray(inverted_image)
+        return img_conv
+    except Exception as e:
+        print(f"Error: {e}. Terjadi kesalahan saat melakukan invers pada gambar.")
+
+# Closing
+def closing(image, kernel_size=(5, 5)):
+    try:
+        kernel = np.ones(kernel_size, np.uint8)
+        closed_image = cv2.morphologyEx(image, cv2.MORPH_CLOSE, kernel)
+        img_conv = Image.fromarray(closed_image)
+        return img_conv
+    except Exception as e:
+        print(f"Error: {e}. Terjadi kesalahan saat melakukan closing pada gambar.")
+
+# Dilasi
+def dilation(image, kernel_size=(5, 5)):
+    try:
+        kernel = np.ones(kernel_size, np.uint8)
+        dilated_image = cv2.dilate(image, kernel, iterations=1)
+        img_conv = Image.fromarray(dilated_image)
+        return img_conv
+    except Exception as e:
+        print(f"Error: {e}. Terjadi kesalahan saat melakukan dilasi pada gambar.")
+
+# Erosi
+def erosion(image, kernel_size=(5, 5)):
+    try:
+        kernel = np.ones(kernel_size, np.uint8)
+        eroded_image = cv2.erode(image, kernel, iterations=1)
+        img_conv = Image.fromarray(eroded_image)
+        return img_conv
+    except Exception as e:
+        print(f"Error: {e}. Terjadi kesalahan saat melakukan erosi pada gambar.")
+
+# Opening
+def opening(image, kernel_size=(5, 5)):
+    try:
+        kernel = np.ones(kernel_size, np.uint8)
+        opened_image = cv2.morphologyEx(image, cv2.MORPH_OPEN, kernel)
+        img_conv = Image.fromarray(opened_image)
+        return img_conv
+    except Exception as e:
+        print(f"Error: {e}. Terjadi kesalahan saat melakukan opening pada gambar.")
--- Frontend.py
+++ Frontend.py
@@ -4,79 +4,128 @@ import cv2
 import numpy as np
 from Backend import *
 
-#--------------implement Screen-------------
-#--------First upload image-----------------
-#-----------Convert image to array----------
-#---------Second Choose Filter from box-----
-
+# Fungsi utama untuk menjalankan aplikasi
 def main():
-    st.header("Selamat datang di aplikasi Pengolahan Citra!")
-    image_upload = st.file_uploader('Silahkan upload gambar...',type=['jpg','png','jpeg'])
-    if image_upload is not None:
-        image = Image.open(image_upload)
-        image_cv2 = np.array(image)
+    # Menampilkan judul aplikasi
+    st.header("Selamat datang di PixelPlay!")
+    
+    # Mengunggah gambar
+    image_upload = st.file_uploader('Silahkan upload gambar...', type=['jpg', 'png', 'jpeg'])
+    
+    # Memeriksa apakah gambar telah diunggah
+    if image_upload is None:
+        st.warning('Silakan pilih file gambar yang valid.')
+    else:
+        try:
+            # Membuka gambar dan mengonversinya ke array NumPy
+            image = Image.open(image_upload)
+            image_cv2 = np.array(image)
+            image_cv2 = cv2.cvtColor(image_cv2, cv2.COLOR_BGR2BGRA)
+            
+            # Memilih filter yang akan digunakan
+            option = st.selectbox('Pilih filter yang akan digunakan', ('Edge Detection', 'Grayscale', 'Negative Transformation', 'Gaussian Blur', 'Reduce Noise', 'Sharping', 'Emboising', 'High Boost', 'Low Pass', 'Rotasi', 'Translasi', 'Countour', 'Invers', 'Closing', 'Dilasi', 'Erosi', 'Opening'))
+            
+            if option:
+                st.write('Kamu memilih:', option)
+                
+                # Tambahkan deskripsi filter
+                if option == 'Edge Detection':
+                    st.write('Filter ini digunakan untuk mendeteksi tepi pada gambar.')
+                elif option == 'Grayscale':
+                    st.write('Filter ini mengubah gambar ke skala abu-abu.')
+                elif option == 'Negative Transformation':
+                    st.write('Filter ini menghasilkan transformasi negatif dari gambar.')
+                elif option == 'Gaussian Blur':
+                    st.write('Filter ini digunakan untuk memberikan efek blur pada gambar.')
+                elif option == 'Reduce Noise':
+                    st.write('Filter ini digunakan untuk mengurangi noise pada gambar.')
+                elif option == 'Sharping':
+                    st.write('Filter ini digunakan untuk mempertajam gambar.')
+                elif option == 'Emboising':
+                    st.write('Filter ini digunakan untuk menambahkan efek embos pada gambar.')
+                elif option == 'High Boost':
+                    st.write('Filter ini digunakan untuk menambahkan efek kecerahan pada gambar.')
+                elif option == 'Low Pass':
+                    st.write('Filter ini digunakan untuk menambahkan efek filter low pass pada gambar.')
+                elif option == 'Rotasi':
+                    st.write('Filter ini digunakan untuk memutar gambar.')
+                    # Memungkinkan pengguna memasukkan sudut rotasi
+                    rotation_angle = st.slider('Pilih sudut rotasi:', -180, 180, 0)
+                elif option == 'Translasi':
+                    st.write('Filter ini digunakan untuk mentranslasikan gambar.')
+                elif option == 'Countour':
+                    st.write('Filter ini digunakan untuk mengekstraksi kontur pada gambar.')
+                elif option == 'Invers':
+                    st.write('Filter ini menghasilkan invers dari gambar.')
+                elif option == 'Closing':
+                    st.write('Filter ini digunakan untuk operasi closing pada gambar.')
+                elif option == 'Dilasi':
+                    st.write('Filter ini digunakan untuk operasi dilasi pada gambar.')
+                elif option == 'Erosi':
+                    st.write('Filter ini digunakan untuk operasi erosi pada gambar.')
+                elif option == 'Opening':
+                    st.write('Filter ini digunakan untuk operasi opening pada gambar.')
+                
+                # Menampilkan gambar asli
+                st.header('Input image')
+                st.image(image)
+                
+                # Menerapkan filter yang dipilih
+                with st.spinner(f"Applying {option}..."):
+                # Periksa apakah opsi adalah 'Rotasi' dan panggil fungsi rotation dengan sudut yang dipilih
+                    if option == 'Rotasi':
+                        processed_image = apply_filter(option, image_cv2, rotation_angle)
+                    else:
+                        processed_image = apply_filter(option, image_cv2)
+                
+                # Menampilkan gambar setelah diterapkan filter
+                st.markdown(f'Image after {option}')
+                st.image(processed_image)
+        except Exception as e:
+            st.error(f'Error: {e}. Pastikan file yang diunggah adalah gambar.')
 
-#------------convert BGR to RGB-------------
-
-        image_cv2 = cv2.cvtColor(image_cv2,cv2.COLOR_BGR2BGRA)
-
-        option = st.selectbox('Pilih filter yang akan digunakan',('Select','Edge Detection','Grayscale','Negative Transformation','Gaussian Blur','Reduce Noise','Sharping'))
-        st.write('Kamu memilih:',option)
-
-#---------Choose from selectbox------------
-
-        if option == 'Select':
-            pass
-
-#--------Edge Detection in selectionbox---
-        elif option == 'Edge Detection':
-            st.header('Input image')
-            st.image(image)
-            st.markdown('Image after Edge Detection')
-            st.image(edge_detection(image_cv2))
-
-# --------Gray Scale in selectionbox-----
-
-        elif option == 'Grayscale':
-            st.header('Input image')
-            st.image(image)
-            st.markdown('Image after Grayscale')
-            st.image(gray_scale(image_cv2))
-
-#------Negative Transformationin selectionbox----------
-
-        elif option == 'Negative Transformation':
-            st.header('Input image')
-            st.image(image)
-            st.markdown('Image after Negative Transformation')
-            st.image(negative_transformation(image_cv2))
-
-#------Gaussian Blur in selectionbox-------------------
-
-        elif option =='Gaussian Blur':
-            st.header('Input image')
-            st.image(image)
-            st.markdown('Image after Gaussian Blurring')
-            st.image(Gaussian_Blur(image_cv2))
-
-#--------Reduce Noise in selectionbox----------------
-
-        elif option =='Reduce Noise':
-            st.header('Input image')
-            st.image(image)
-            st.markdown('Image after Remove Noise')
-            st.image(reduce_noise(image_cv2))
-
-#-----------Sharping in selectionbox----------------
-
-        elif option =='Sharping':
-            st.header('Input image')
-            st.image(image)
-            st.markdown('Image after Sharping')
-            st.image(sharp_image(image_cv2))
-
+# Fungsi untuk menerapkan filter yang dipilih
+def apply_filter(option, image_cv2, rotation_angle=None):
+    if option == 'Edge Detection':
+        return edge_detection(image_cv2)
+    elif option == 'Grayscale':
+        return gray_scale(image_cv2)
+    elif option == 'Negative Transformation':
+        return negative_transformation(image_cv2)
+    elif option == 'Gaussian Blur':
+        return Gaussian_Blur(image_cv2)
+    elif option == 'Reduce Noise':
+        return reduce_noise(image_cv2)
+    elif option == 'Sharping':
+        return sharp_image(image_cv2)
+    elif option == 'Emboising':
+        return emboss(image_cv2)
+    elif option == 'High Boost':
+        return high_boost(image_cv2)
+    elif option == 'Low Pass':
+        return low_pass(image_cv2)
+    elif option == 'Rotasi':
+        # Periksa apakah sudut rotasi telah diberikan
+        if rotation_angle is not None:
+            return rotation(image_cv2, rotation_angle)
         else:
-            pass
+            return image_cv2
+    elif option == 'Translasi':
+        return translation(image_cv2)
+    elif option == 'Countour':
+        return contour(image_cv2)
+    elif option == 'Invers':
+        return inverse(image_cv2)
+    elif option == 'Closing':
+        return closing(image_cv2)
+    elif option == 'Dilasi':
+        return dilation(image_cv2)
+    elif option == 'Erosi':
+        return erosion(image_cv2)
+    elif option == 'Opening':
+        return opening(image_cv2)
 
-if __name__ =="__main__":
+# Menjalankan aplikasi jika file dieksekusi langsung
+if __name__ == "__main__":
     main()
+
